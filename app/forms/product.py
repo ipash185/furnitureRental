188bc8f6121f8f613af4e57bc64854e7b0e0bdda
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-03-17 23:34:53 +0530

    initial

---
+++ app/forms/product.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from app.models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'quantity'
+            'brand',
+            'image',
+            'available',
+        ]
+
