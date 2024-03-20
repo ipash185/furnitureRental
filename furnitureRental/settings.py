5da12e4e0fbe8f6bf4d5bce2c2cef7ce74f7fae8
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-03-20 10:58:00 +0530

    Update settings.py

--- furnitureRental/settings.py
+++ furnitureRental/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'furnitureRental.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newDB',
-        'USER': 'postgres',
-        'PASSWORD': '0911',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
