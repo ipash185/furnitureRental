aa70b6bc103e426c386d5e17ce7fba68f499511c
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-03-26 12:54:59 +0530

    favicon

--- furnitureRental/settings.py
+++ furnitureRental/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "123"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost", "furniturerental.store"]
 
