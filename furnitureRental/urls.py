048c5ac708b5e388297da0877e36fb8835cf8ec2
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-03-26 13:09:10 +0530

    icon

--- furnitureRental/urls.py
+++ furnitureRental/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('app.urls')),
+    path('favicon.png', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.png')))
 ]
 
