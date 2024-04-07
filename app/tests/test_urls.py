23cc97634e65c0307194d4b74031ae64e8505731
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-04-07 11:23:59 +0530

    Update test_urls.py

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -7,4 +7,10 @@ from app.urls import urlpatterns
 class MyAppUrlsTest(test.SimpleTestCase):
 
     def test_responses(self):
-        self.assertTrue(1==2)
+        for url in urlpatterns:
+            # For now, perform only GET requests and ignore URLs that need arguments.
+            if not isinstance(url, URLPattern) or url.pattern.regex.groups or not url.name:
+                continue
+            urlpath = reverse(url.name)
+            response = self.client.get(urlpath, follow=True)
+            self.assertEqual(response.status_code, 200)
